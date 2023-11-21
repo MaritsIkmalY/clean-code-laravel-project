0f14af92cae473ea10e47f1feb482b0aec8193a7
MaritsIkmalY <maritsikmaly@gmail.com>
2023-11-21 19:03:51 +0700

    add : nutrition measurement type crud

---
+++ app/Services/NutritionMeasurementTypeService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services;
+
+use App\Data\NutritionMeasurementTypeData;
+use App\Http\Resources\NutritionMeasurementTypeResource;
+use App\Models\NutritionMeasurementType;
+
+class NutritionMeasurementTypeService
+{
+    public function index()
+    {
+        $nutritionMeasurementType = NutritionMeasurementType::paginate(10);
+        return NutritionMeasurementTypeResource::collection($nutritionMeasurementType);
+    }
+
+    public function show(NutritionMeasurementType $nutritionMeasurementType)
+    {
+        return NutritionMeasurementTypeResource::make($nutritionMeasurementType);
+    }
+
+    public function create(NutritionMeasurementTypeData $nutritionMeasurementTypeData)
+    {
+        $nutritionMeasurementType = NutritionMeasurementType::create($nutritionMeasurementTypeData->toArray());
+
+        return $this->createNutritionMeasurementTypeResponse(
+            $nutritionMeasurementType,
+            'Nutrition measurement type created successfully'
+        );
+    }
+
+    public function update(
+        NutritionMeasurementTypeData $nutritionMeasurementTypeData,
+        NutritionMeasurementType $nutritionMeasurementType
+    ) {
+        $nutritionMeasurementType->update($nutritionMeasurementTypeData->toArray());
+
+        return $this->createNutritionMeasurementTypeResponse(
+            $nutritionMeasurementType,
+            'Nutrition measurement type updated successfully'
+        );
+    }
+
+    public function destroy(NutritionMeasurementType $nutritionMeasurementType)
+    {
+        $nutritionMeasurementType->delete();
+        return $this->createNutritionMeasurementTypeResponse(
+            $nutritionMeasurementType,
+            'Nutrition measurement type deleted successfully'
+        );
+    }
+
+    protected function createNutritionMeasurementTypeResponse(
+        NutritionMeasurementType $nutritionMeasurementType,
+        string $message
+    ) {
+        return NutritionMeasurementTypeResource::make($nutritionMeasurementType)
+            ->additional(['message' => $message])
+            ->response();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\NutritionMeasurementTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function(){
     Route::resource('/food', FoodController::class);
+    Route::resource('/nutrition-measurement-type', NutritionMeasurementTypeController::class);
 });
